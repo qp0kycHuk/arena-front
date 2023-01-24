56a0a37e0aecdab9cc0a2d9bc462bd8d1b4c1f9e
qp0kycHuk <serega990112@mail.ru>
2023-01-24 17:33:41 +0300

    change error text

--- src/const/Text.ts
+++ src/const/Text.ts
@@ -1,2 +1,2 @@
-export const SERVER_ERROR_MESSAGE = 'Что то пошло не так'
 export const TRY_AGAIN = 'Попробуйте снова'
+export const SERVER_ERROR_MESSAGE = 'Что то пошло не так, ' + TRY_AGAIN.toLowerCase()
