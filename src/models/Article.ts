ea378a8fcb21bd4ade2d31aa2e3fee5ba12e9d07
qp0kycHuk <serega990112@mail.ru>
2023-01-30 16:54:32 +0300

    add image_src field

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -2,15 +2,16 @@ import { ICategory } from "./Category";
 import { ITag } from "./Tag";
 
 export interface IArticle {
-    id: number;
-    user_id: number;
-    name: string;
-    content: string;
-    excerpt: string;
-    image: string | null;
-    created_at: Date;
-    updated_at: Date;
-    categories: ICategory[];
-    tags: ITag[];
-    files: any[];
+    id: number
+    user_id: number
+    name: string
+    content: string
+    excerpt: string
+    image: string | null
+    created_at: Date
+    updated_at: Date
+    image_src: string
+    categories: ICategory[]
+    tags: ITag[]
+    files: any[]
 }
