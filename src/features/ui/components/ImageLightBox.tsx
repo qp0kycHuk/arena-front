b0babf9c6bae62bfa72a31b5f35838469b60ef9f
qp0kycHuk <serega990112@mail.ru>
2023-02-02 11:15:57 +0300

    fix image viewer height

--- src/features/ui/components/ImageLightBox.tsx
+++ src/features/ui/components/ImageLightBox.tsx
@@ -13,15 +13,13 @@ export function ImageLightBox({ children, src }: IImageLightBoxProps) {
 
     return (
         <>
-            <div onClick={() => setIsOpen(true)}>
+            <div className="cursor-pointer" onClick={() => setIsOpen(true)}>
                 {children}
             </div>
             {isOpen && createPortal((
                 <div className="fixed inset-0 bg-black bg-opacity-80 z-40 p-4 flex">
                     <div className="fixed inset-0 z-10" onClick={() => setIsOpen(false)}></div>
-                    <div className="m-auto max-w-full z-20">
-                        <img src={src} alt="" className=" max-h-full max-w-full" />
-                    </div>
+                    <img src={src} alt="" className="m-auto max-h-full max-w-full z-20" />
                     <Button variant="text" icon className="fixed right-1 top-1 z-30" onClick={() => setIsOpen(false)}>
                         <CrossIcon className="text-white" />
                     </Button>
