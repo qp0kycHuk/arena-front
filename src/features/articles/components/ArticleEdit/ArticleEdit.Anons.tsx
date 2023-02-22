80391189d33199a978bff65737e4c556f305a750
qp0kycHuk <serega990112@mail.ru>
2023-02-22 13:55:51 +0300

    fix key prop

--- src/features/articles/components/ArticleEdit/ArticleEdit.Anons.tsx
+++ src/features/articles/components/ArticleEdit/ArticleEdit.Anons.tsx
@@ -1,21 +1,15 @@
 import { useMemo } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useArticleControl } from '@store/articles';
 import { Uploader } from '@features/fileUploader';
-import { useArticleEditMainContext, useArticleEditUtilsContext } from './ArticleEdit.Context';
+import { useArticleEditMainContext } from './ArticleEdit.Context';
 import { getFilePreview, getRandomUUID } from '@utils/index';
-import { ICreateRequest } from '@store/articles/articles.api';
 
 export interface IArticleEditAnonsProps { }
 
 export function ArticleEditAnons({ }: IArticleEditAnonsProps) {
     const { article, update } = useArticleEditMainContext()
-    const { loadingStart, loadingEnd, getFormData } = useArticleEditUtilsContext()
-    const { upsertArticle, createDraftArticle } = useArticleControl()
-    const navigate = useNavigate();
 
     const fileItems = useMemo(() => article?.image_src ? ([{
-        id: article?.id,
+        id: article?.id || getRandomUUID(),
         src: article?.image_src,
         // title: article?.image,
     }]) : [], [article])
