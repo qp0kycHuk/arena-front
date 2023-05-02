bff72bf5b321410a33e64acf30bd2badf8e8cd71
qp0kycHuk <serega990112@mail.ru>
2023-05-02 12:47:10 +0300

    some fix

--- src/store/articles/articles.api.ts
+++ src/store/articles/articles.api.ts
@@ -1,7 +1,7 @@
 import type { IArticle } from "@models/Article";
 import { EntitesApi } from '@store/utils/EntitesApi';
 
-type CreateParams = 'owner_id' | 'content' | 'excerpt' | 'name' | 'image' | 'tags[]' | 'attachment[]' | 'parent_id'
+type CreateParams = 'owner_id' | 'content' | 'excerpt' | 'name' | 'image' | 'tags[]' | 'attachment[]' | 'parent_id' | 'folders[]'
 type UpdateParams = CreateParams | 'id' | 'image_delete'
 export type ICreateRequest = TypedFormData<CreateParams>
 export type IUpdateRequest = TypedFormData<UpdateParams>
