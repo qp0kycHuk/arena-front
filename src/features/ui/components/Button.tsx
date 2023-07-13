5991a7dff1775a8fc3e286057c0b2f3d9218b3c6
qp0kycHuk <serega990112@mail.ru>
2023-07-13 10:35:17 +0300

    change buttons to tailwind plugin

--- src/features/ui/components/Button.tsx
+++ src/features/ui/components/Button.tsx
@@ -18,32 +18,30 @@ interface IProps {
 export type ButtonProps = IProps & Omit<React.ButtonHTMLAttributes<HTMLElement>, keyof IProps>
 export type ButtonRef = React.ForwardedRef<HTMLElement>
 
-const baseClassNames = 'flex items-center cursor-pointer ring-0 font-medium transition overflow-hidden outline-none disabled:pointer-events-none disabled:opacity-50 focus:border-transparent active:translate-y-0.5'
+const baseClassNames = 'btn'
 
 const sizeClassNames: Record<Size, string> = {
-  xsmall: 'h-7',
-  small: 'h-[38px]',
-  middle: 'h-12',
-  large: 'h-14',
+  xsmall: 'btn-xs',
+  small: 'btn-sm',
+  middle: 'btn-md',
+  large: 'btn-lg',
 };
 
-const iconSizeClassNames: Record<Size, string> = {
-  xsmall: 'min-w-7 w-7',
-  small: 'min-w-[38px] w-[38px]',
-  middle: 'min-w-12 w-12',
-  large: 'min-w-14 w-14',
-};
 
 const colorClassNames: PartialRecord<Color, string> = {
-  gray: 'dark:text-white focus:ring-primary focus:border-primary',
+  primary:'btn-primary',
+  gray:'btn-gray',
+  red:'btn-red',
+  green:'btn-green',
+  yellow:'btn-yellow',
 };
 
 const variantClassNames: Record<Variant, string> = {
-  fill: `text-white ring-opacity-10 focus:ring-4 ripple--light active:ring-0`,
-  whitebg: `bg-white hover:bg-gray-50  dark:bg-gray-900 dark:hover:bg-gray-800 ring-0 focus:ring-1 active:ring-0`,
-  light: `bg-opacity-20 hover:bg-opacity-30 focus:ring-2 active:ring-0`,
-  contur: `border border-opacity-30 ring-0 hover:bg-opacity-10 focus:ring-1 active:ring-0`,
-  text: `bg-opacity-0 focus:bg-opacity-10 focus:ring-1 active:ring-0 hover:bg-opacity-10`,
+  fill: `btn-fill`,
+  whitebg: `btn-whitebg`,
+  light: `btn-light`,
+  contur: `btn-contur`,
+  text: ``,
 };
 
 
@@ -59,22 +57,19 @@ function ButtonComponent({
   icon = false,
   ...props }: ButtonProps, ref: ButtonRef) {
 
-  const variantColorClassNames: Record<Variant, string> = {
-    fill: `bg-${color} hover:bg-${color}-600`,
-    whitebg: `text-${color}`,
-    light: `text-${color} bg-${color}`,
-    contur: `text-${color} border-${color} hover:bg-${color}`,
-    text: `text-${color} bg-${color}`,
-  }
-
-  const extraClassNames = [
+  const classNames = [
+    baseClassNames,
+        sizeClassNames[size],
+        variantClassNames[variant],
+        colorClassNames[color],
     rounded ? 'rounded-full' : 'rounded-lg',
     shadow ? 'shadow-md' : '',
-    icon ? iconSizeClassNames[size] : '',
+    icon ? 'btn-icon' : '',
     (icon || props.className?.includes('px-')) ? '' : props.className?.includes('pl-') ? '' : 'pl-4',
     (icon || props.className?.includes('px-')) ? '' : props.className?.includes('pr-') ? '' : 'pr-4',
     props.className?.includes('justify-') ? '' : 'justify-center',
-    `ring-${color}`
+    props.className
+
 
   ].join(' ')
 
@@ -85,15 +80,7 @@ function ButtonComponent({
       type="button"
       {...props}
       onPointerDown={ripplePointerdownHandler}
-      className={[
-        baseClassNames,
-        sizeClassNames[size],
-        variantColorClassNames[variant],
-        variantClassNames[variant],
-        colorClassNames[color],
-        extraClassNames,
-        props.className
-      ].join(' ')}>
+      className={classNames}>
       {children}
     </ButtonTag >
   );
