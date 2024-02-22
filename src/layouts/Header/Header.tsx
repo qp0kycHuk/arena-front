1d5dcd79e0f9a256a81b62b130965eaf2edd057a
qp0kycHuk <serega990112@mail.ru>
2024-02-22 11:56:02 +0300

    add burger

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -2,15 +2,20 @@ import { Link } from 'react-router-dom'
 import { useAuth } from '@store/auth'
 import { Button } from '@features/ui'
 import { HeaderUser } from './HeaderUser'
-import { BookmarkIcon } from '@assets/icons/stroke'
+import { BookmarkIcon, MenuIcon } from '@assets/icons/stroke'
 import headerLogo from '@assets/img/header-logo.png'
 import { ThemeToggle } from '@/components/ThemeToggle/ThemeToggle'
+import { useSidebarContext } from '../Sidebar/SidebarContext'
 
 export function Header() {
   const { data: auth } = useAuth()
+  const { isOpen, toggleSidebar } = useSidebarContext()
 
   return (
-    <header className="sticky top-0 z-20 flex items-center px-8 py-2 bg-l3 shadow-sm ">
+    <header className="sticky top-0 z-20 flex items-center px-3 xl:px-8 py-2 bg-l3 shadow-sm ">
+      <Button className="xl:hidden mr-3 -ml-2" size="sm" variant="text" onClick={toggleSidebar}>
+        <MenuIcon className="text-2xl" />
+      </Button>
       <div className="mr-auto">
         <img src={headerLogo} alt="" className="h-[26px]" />
       </div>
--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -1,12 +1,4 @@
-import {
-  BriefcaseIcon,
-  CrownIcon,
-  DocumentIcon,
-  FoldersIcon,
-  HandbooksIcon,
-  ToTopIcon,
-  UsersIcon,
-} from '@assets/icons/stroke'
+import { CrossIcon, CrownIcon, HandbooksIcon, UsersIcon } from '@assets/icons/stroke'
 import { Button } from '@features/ui'
 import { getRoute } from '@utils/index'
 import { SidebarButton } from './SidebarButton'
@@ -14,6 +6,7 @@ import { Tags } from './Sidebar.Tags'
 import { Folders } from './Sidebar.Folders'
 import { useSidebarContext } from './SidebarContext'
 import classNames from 'classnames'
+import headerLogo from '@assets/img/header-logo.png'
 
 export function Sidebar() {
   const { isOpen, closeSidebar } = useSidebarContext()
@@ -21,20 +14,30 @@ export function Sidebar() {
   return (
     <>
       <div className={classNames('sidebar', isOpen ? 'active' : null)}>
-        <div className="space-y-1">
-          <SidebarButton title="Главная" icon={CrownIcon} link="/" />
-          <SidebarButton title="Пользователи" icon={UsersIcon} link={getRoute().users()} />
-          {/* <SidebarButton title="Статьи" icon={DocumentIcon} link={getRoute().articles()} /> */}
-          <SidebarButton title="Справочник" icon={HandbooksIcon} link="handbooks" />
+        <header className="sticky top-0 z-20 flex items-center px-3 xl:px-8 py-2 bg-l3 shadow-sm ">
+          <Button className="xl:hidden mr-3 -ml-2" size="sm" variant="text" onClick={closeSidebar}>
+            <CrossIcon className="text-2xl" />
+          </Button>
+          <div className="mr-auto">
+            <img src={headerLogo} alt="" className="h-[26px]" />
+          </div>
+        </header>
+        <div className="sidebar-content">
+          <div className="space-y-1">
+            <SidebarButton title="Главная" icon={CrownIcon} link="/" />
+            <SidebarButton title="Пользователи" icon={UsersIcon} link={getRoute().users()} />
+            {/* <SidebarButton title="Статьи" icon={DocumentIcon} link={getRoute().articles()} /> */}
+            <SidebarButton title="Справочник" icon={HandbooksIcon} link="handbooks" />
 
-          <Folders />
+            <Folders />
+          </div>
+          <div className="mt-3 mb-5 border-t border-default/20"></div>
+
+          <Tags />
+          <Button variant="text" size="sm" className="justify-start w-auto mt-5 -ml-2 -mr-2">
+            Показать всё
+          </Button>
         </div>
-        <div className="mt-3 mb-5 border-t border-default/20"></div>
-
-        <Tags />
-        <Button variant="text" size="sm" className="justify-start w-auto mt-5 -ml-2 -mr-2">
-          Показать всё
-        </Button>
       </div>
       <div className="sidebar-shadow" onClick={closeSidebar}></div>
     </>
